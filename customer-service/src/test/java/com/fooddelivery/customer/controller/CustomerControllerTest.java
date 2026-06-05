22679eb49f1c11d60abc528edd40d8be7a9f1cac
Tejes Dombe <tejes.dombe@successive.tech>
2026-06-05 11:04:24 +0530

    Build

--- customer-service/src/test/java/com/fooddelivery/customer/controller/CustomerControllerTest.java
+++ customer-service/src/test/java/com/fooddelivery/customer/controller/CustomerControllerTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,8 +23,8 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(CustomerController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @DisplayName("Customer Controller Integration Tests")
 class CustomerControllerTest {
 
